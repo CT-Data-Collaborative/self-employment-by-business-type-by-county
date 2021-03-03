94a9bb09705b9961254efaa5ce0a166b33933e94
ilyankou <ilyankou@gmail.com>
2021-03-03 08:43:52 +0000

    Update ACS 2019 5y

--- scripts/self-employment-by-business-type-by-county-processing.R
+++ scripts/self-employment-by-business-type-by-county-processing.R
@@ -23,7 +23,7 @@ path_to_raw <- (paste0(getwd(), "/", raw_location))
 options(scipen=999)
 acsdata <- getACSData(
     getCTGeos("town"),
-    yearList = 2010:2018,
+    yearList = 2010:2019,
     table = "S2406"
 )
 
@@ -130,7 +130,7 @@ business_type_final <- business_type_fips %>%
     
 write.table(
     business_type_final,
-    file.path("data", "self-employment-business-county-2018.csv"),
+    file.path("data", "self-employment-business-county-2019.csv"),
     sep = ",",
     row.names = F,
     col.names = T,
